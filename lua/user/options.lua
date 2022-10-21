846e1716e62c02cd16996c87f11d5d0f9a264132
Cole Wyant <cwyant314159@gmail.com>
2022-10-20 21:56:57 -0500

    Initial commit. WIP on option doc

---
+++ lua/user/options.lua
@@ -0,0 +1,42 @@
+local options = {
+    backup = false,
+    clipboard = "unnamedplus",
+    cmdheight = 2,
+    completeopt = { "menuone", "noselect" },
+    conceallevel = 0,
+    fileencoding = "utf-8",
+    hlsearch = true,
+    ignorecase = true,
+    mouse = "a",
+    pumheight = 10,
+    showmode = false,
+    showtabline = 2,
+    smartcase = true,
+    smartindent = true,
+    splitbelow = true,
+    splitright= true,
+    swapfile = false,
+--    termguicolors = true,
+    timeoutlen = 1000,
+    undofile = true,
+    updatetime = 300,
+    writebackup = false,
+    expandtab = true,
+    shiftwidth = 4,
+    tabstop = 4,
+    softtabstop = 4,
+    cursorline = true,
+    number = true,
+    relativenumber = true,
+    signcolumn = "yes",
+    wrap = false,
+    scrolloff = 8,
+    sidescrolloff = 8,
+    colorcolumn = "120",
+    shortmess = vim.opt.shortmess:append "c",
+    whichwrap = vim.opt.whichwrap:append ",<,>,[,],h,l"
+}
+
+for k, v in pairs(options) do
+    vim.opt[k] = v
+end
